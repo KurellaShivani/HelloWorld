1131ec1943236eb3c9566d466920ddac415c0cba
cheed <cheed@cheeday>
2020-10-18 18:59:23 +0530

    Initial commit

---
+++ helloworld/src/main/java/com/project/helloworld/HelloworldApplication.java
@@ -0,0 +1,14 @@
+package com.project.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+		System.out.println("Hellooo......");
+	}
+
+}
